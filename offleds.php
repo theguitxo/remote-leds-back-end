c2ad1ae8b40f0d6d7bbe77899e35860b4d4d0437
Amador <guitxo@guitxo.com>
2019-09-21 23:48:22 +0200

    updates in php scripts

--- offleds.php
+++ offleds.php
@@ -6,16 +6,39 @@
 
   // opens the file that contains the process id
   // of python script that is running the leds sequence
-  $pidfile = 'leds.pid.txt';
-  $fp = fopen($pidfile, 'r');
-  $pid = fread($fp, filesize($pidfile));
-  fclose($fp);
+  try {
+    $pidfile = 'leds.pid.txt';
+    $fp = fopen($pidfile, 'r');
+    if(!$fp) {
+      die(json_encode(array('result' => false, 'message' => 'error getting pid')));
+    }    
+    $pid = fread($fp, filesize($pidfile));
+    fclose($fp);
+  } catch (Exception $e) {
+    die(json_encode(array('result' => false, 'message' => $e->getMessage())));
+  }
 
   // it executes a command to kill the process
-  $command = escapeshellcmd('sudo kill -9 ' . $pid);
-  exec($command);
+  try {
+    $command = 'sudo kill -9 ' . $pid;
+    system($command, $result);
+    if($result > 0) {
+      die(json_encode(array('result' => false, 'message' => 'error on kill process')));  
+    }
+  } catch (Exception $e) {
+    die(json_encode(array('result' => false, 'message' => $e->getMessage())));
+  }
 
   // it executes a python script that get off all the leds
-  $command = escapeshellcmd('sudo python3 ledsoff.py');
-  exec($command);
+  try {    
+    $command = 'sudo python3 ledsoff.py';
+    system($command, $result);    
+    if($result > 0) {
+      die(json_encode(array('result' => false, 'message' => 'error on turn off leds')));  
+    }
+  } catch (Exception $e) {
+    die(json_encode(array('result' => false, 'message' => $e->getMessage())));
+  }
+
+  echo json_encode(array('result' => true));
 ?>
--- onleds.php
+++ onleds.php
@@ -5,17 +5,43 @@
   // error_reporting(E_ALL);
 
   // gets, from the received post data, the information to play the sequence
-  $data = $_POST['data'];
-  $steps = json_decode($_POST['data']);
-  $objectForFile =  array('steps' => $steps);
+  if(!array_key_exists('data', $_POST)) {
+    die(json_encode(array('result' => false, 'message' => 'no data key')));
+  } else {
+    $data = $_POST['data'];
+    $steps = json_decode($_POST['data']);
+    $objectForFile =  array('steps' => $steps);
+  } 
 
   // saves the information into a json file, this file is used
   // by the python script that play the sequence
-  $fp = fopen('data.json', 'w');
-  fwrite($fp, json_encode($objectForFile));
-  fclose($fp);
+  try {
+    $fp = fopen('data.json', 'w');
+    if(!$fp) {
+      die(json_encode(array('result' => false, 'message' => 'error encoding data')));
+    }    
+    $result = fwrite($fp, json_encode($objectForFile));
+    if(!$result) {
+      die(json_encode(array('result' => false, 'message' => 'error saving data')));
+    }
+    fclose($fp);
+  } catch(Exception $e) {
+    // exception when trying to save JSON file with data
+    die(json_encode(array('result' => false, 'message' => $e->getMessage())));
+  }
 
-  // execute the python script,
-  $command = escapeshellcmd('sudo python3 ledson.py > /dev/null');
-  exec($command);
+  // execute the python script,        
+  try {   
+    $command = 'sudo python3 ledson.py >/dev/null &';
+    system($command, $result);  
+    if($result > 0) {
+      die(json_encode(array('result' => false, 'message' => 'error on turn on leds')));
+    }
+  } catch(Exception $e) {
+    // exception when trying to execute python script
+    die(json_encode(array('result' => false, 'message' => $e->getMessage())));
+  }
+  
+  // all OK, sends true to front-end
+  echo json_encode(array('result' => true));   
 ?>
